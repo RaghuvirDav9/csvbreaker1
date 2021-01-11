3c8f72277e6f5aa02797a155e0eb7c6f21db0263
RaghuvirDav9 <59292224+RaghuvirDav9@users.noreply.github.com>
2021-01-11 09:05:22 +0000

    Update list_ec2.py
    used click library to list running state instances ,passing 'running' as command argument

--- list_ec2.py
+++ list_ec2.py
@@ -1,14 +1,28 @@
 import boto3
+import click
 
 """     this function connects to aws using boto3 and saved configuration for aws 
         and lists the name and id of all instances running in default region            """
 
 
-def listInstanceNameAWS():
+@click.command()
+@click.option("--state", default='ALL', help="Number of greetings.")
+def listInstanceNameAWS(state: str) -> None:
     ec2 = boto3.resource('ec2')
-    for instance in ec2.instances.all():
-        print('id: ', instance.id)
-        print('name:', instance.tags[0]['Value'])
+    if state == 'running':
+        for instance in ec2.instances.all():
+            if instance.state['Name'] == 'running':
+                print('Printing just running instances')
+                print('id: ', instance.id)
+                print('name:', instance.tags[0]['Value'])
+                print('state:',instance.state['Name'])
+                print()
+    else:
+        for instance in ec2.instances.all():
+            print('id: ', instance.id)
+            print('state:',instance.state['Name'])
+            print('name:', instance.tags[0]['Value'])
+            print()
 
 
 listInstanceNameAWS()
