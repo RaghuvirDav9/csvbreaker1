c523c83fb160a83ca230dbb8fbe447eee54b3930
RaghuvirDav9 <59292224+RaghuvirDav9@users.noreply.github.com>
2021-01-11 05:58:48 +0000

    Update activemqproducer.py
    updated the code

--- activemqproducer.py
+++ activemqproducer.py
@@ -1,10 +1,15 @@
 import stomp
 import random
 
-q=random.Random()
 
-conn = stomp.Connection([('localhost', 61613)])
-conn.connect('admin', 'password', wait=True)
-message='hello from producer 1 number'+str(q.random())
-conn.send(body=message, destination='/queue/test')
-conn.disconnect()
+# This function connects to activemq using stomp and pushes message to the queue 
+def activeMQprod():
+    q = random.Random()
+    conn = stomp.Connection([('localhost', 61613)])
+    conn.connect('admin', 'password', wait=True)
+    message = 'hello from producer 1 number' + str(q.random())
+    conn.send(body=message, destination='/queue/test')
+    conn.disconnect()
+
+
+activeMQprod()
